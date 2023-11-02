50767d80fee16745099129ff0015d3a48fbff577
Eniola011 <yettyagbalu@gmail.com>
2023-11-01 18:54:21 -0700

    task17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -15,16 +15,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	}
 
-	binary_tree_t *parent = node->parent;
-
-	if (parent->left == node && parent->right != NULL)
+	if (node->parent->left != node)
 	{
-		return (parent->right);
-	}
-	else if (parent->right == node && parent->left != NULL)
-	{
-		return (parent->left);
+		return (node->parent->left);
 	}
 
-	return (NULL);
+	return (node->parent->right);
 }
